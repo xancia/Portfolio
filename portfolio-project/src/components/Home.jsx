ccfe2b152695a63b4dca4b4ec43c8b9a83fd2597
Xancia <cminhma@gmail.com>
2023-11-29 23:42:06 -0500

    iconify connected, tailwind connect, basic setup finished

--- portfolio-project/src/components/Home.jsx
+++ portfolio-project/src/components/Home.jsx
@@ -1,7 +1,9 @@
+import { Icon } from "@iconify/react"
+
 export default function Home() {
     return (
-        <div className='w-screen h-screen bg-red-300 text-center'>
-            Hello, World. 
+        <div className='w-screen h-screen bg-red-300 flex flex-col items-center'>
+            <Icon className="text-9xl" icon="material-symbols:home"/>
         </div>
     )
 }
