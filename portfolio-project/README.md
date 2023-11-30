d3e2944af53acda06154b5927de211be8d14e4d0
Xancia <cminhma@gmail.com>
2023-11-30 01:43:35 -0500

    restarting

--- portfolio-project/README.md
+++
@@ -1,5 +0,0 @@
-# My Portfolio - Vite & React & Tailwindcss
-
-This is my personal portfolio website I'm currently working on while studying at Per Scholas
-
-
--- portfolio-project/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { Icon } from "@iconify/react"
-
-export default function Home() {
-    return (
-        <div className='w-screen h-screen bg-red-300 flex flex-col items-center'>
-            <Icon className="text-9xl" icon="material-symbols:home"/>
-        </div>
-    )
-}
