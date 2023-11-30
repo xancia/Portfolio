d7933531bfd111c967b0f2ac077394539810293d
Xancia <cminhma@gmail.com>
2023-11-29 23:08:41 -0500

    inital set up vite+react+tailwind

---
+++ portfolio-project/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='w-screen h-screen bg-red-300 text-center'>
+      Hello, World
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,jsx,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
