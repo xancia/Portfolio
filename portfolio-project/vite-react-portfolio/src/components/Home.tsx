5e05e274f61de3ab7e65ffe7d00165d0eff3899c
Xancia <cminhma@gmail.com>
2023-11-30 02:05:03 -0500

    Changed to tsx to use shadecn/ui

---
+++ portfolio-project/vite-react-portfolio/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button"
+
+export default function Home(){
+    return (
+        <div className="flex flex-col items-center w-screen h-screen bg-red-300">
+            Hello, World
+            <Button />
+        </div>
+    )
+}
