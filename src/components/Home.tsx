795c1070892bac9f47227789e8e22e18d7eadb01
Xancia <cminhma@gmail.com>
2023-11-30 16:35:49 -0500

    added new font family from google to tailwind

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,10 @@
-import { Button } from "./ui/button";
-import { Icon } from "@iconify/react";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-screen h-screen bg-red-300">
       Hello, World
-      <Button>Click</Button>
-      <Icon className="text-3xl" icon="material-symbols:home" />
+     
     </div>
   );
 }
