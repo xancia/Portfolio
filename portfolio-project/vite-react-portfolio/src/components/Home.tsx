7e82427119e3b2507ed12179849cc470ba579045
Xancia <cminhma@gmail.com>
2023-11-30 02:16:29 -0500

    added back iconify

--- portfolio-project/vite-react-portfolio/src/components/Home.tsx
+++ portfolio-project/vite-react-portfolio/src/components/Home.tsx
@@ -1,10 +1,12 @@
 import { Button } from "./ui/button"
+import {Icon} from "@iconify/react"
 
 export default function Home(){
     return (
         <div className="flex flex-col items-center w-screen h-screen bg-red-300">
             Hello, World
             <Button />
+            <Icon icon="material-symbols:home"/>
         </div>
     )
 }
