be3b2015be4e8a394757734353e52ed640a26044
Xancia <cminhma@gmail.com>
2023-11-30 21:39:35 -0500

    added about page, in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Home from "./components/Home"
 import { NavBar } from "./components/NavBar"
 import SocialLinks from "./components/SocialLinks"
@@ -8,7 +9,7 @@ function App() {
     <>
       <NavBar />
       <Home />
-
+      <About />
       <SocialLinks />
     </>
   )
