0c741cdd12ac208dff912210d46f8a33493534fc
Xancia <cminhma@gmail.com>
2023-11-30 21:38:01 -0500

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Home from "./components/Home"
+import { NavBar } from "./components/NavBar"
+import SocialLinks from "./components/SocialLinks"
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Home />
+
+      <SocialLinks />
     </>
   )
 }
