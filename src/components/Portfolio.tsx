b7119ec4cbebff97b7f40f4be151b31a1f0763bd
Xancia <cminhma@gmail.com>
2023-11-30 22:01:40 -0500

    building portflio template

---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,30 @@
+import gameImage from '/game.jpg'
+import { Button } from './ui/button'
+
+const Portfolio = () => {
+  return (
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+                <p className='py-6'>Check out some of my work here</p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+
+                <div className='shadow-md shadow-gray-600 rounded-lg'>
+                    <img src={gameImage} alt="my game" />
+                    <div>
+                        <Button>Demo</Button>
+                        <Button>Code</Button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
