925dd1c56513894ebd00b5fb0170cd97c4861c98
Xancia <cminhma@gmail.com>
2024-12-10 19:52:45 -0500

    Updated Hero

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,11 +8,11 @@ export default function Home() {
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
           <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-            I'm a Full Stack Developer
+            .NET Full Stack Developer
           </h2>
           <p className='text-gray-500 py-4 max-w-md'>
             An Emerging Software Engineer looking to advance my career in the industry. 
-            Currently, I build applications using MERN, Tailwind, Typescript, and Vite.
+            Currently, I build applications using .NET Core, ReactJS, Tailwind, Typescript, and Vite.
           </p>
           <div>
             <Button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-teal-500 to-green-500 cursor-pointer' asChild>
